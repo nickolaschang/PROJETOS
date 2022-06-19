92f19ecbe10181b0e743fce4ee450a03cc726f35
Nickolas Alves <nickolaschang@hotmail.com>
2022-06-19 18:52:57 +1000

    New Studies2

---
+++ DemoStringBuilder/DemoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace DemoStringBuilder.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ DemoStringBuilder/DemoStringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DemoStringBuilder.Entities;
+
+namespace DemoStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment1 = new Comment("Have a nice trip!");
+            Comment comment2 = new Comment("Woah, that is awesome!");
+            Comment comment3 = new Comment("Cya!! good luck!");
+            Comment comment4 = new Comment("May the force be with you! :D");
+
+            Post post1 = new Post(DateTime.Parse("19/04/2022 13:55:36"), "Traveling to New Zealand", "Im going to visit this wonderful country!!", 12);
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+            Post post2 = new Post(DateTime.Parse("21/06/2022 13:25:16"), "Have fun!!", "See you tomorrow!!", 5);
+            post1.AddComment(comment3);
+            post1.AddComment(comment4);
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+
+        }
+    }
+}
