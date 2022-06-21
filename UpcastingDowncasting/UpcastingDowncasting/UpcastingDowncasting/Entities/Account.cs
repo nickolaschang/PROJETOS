04060e5638d039fdea729fc32b0ded0314c92e1c
Nickolas Alves <nickolaschang@hotmail.com>
2022-06-21 13:40:22 +1000

    studies 3

--- UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Entities/Account.cs
+++ UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Entities/Account.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -21,9 +21,9 @@ namespace UpcastingDowncasting
             Balance = balance;
         }
 
-        public void Withdraw(double amount)
+        public virtual void Withdraw(double amount)
         {
-            Balance -= amount;
+            Balance -= amount + 5.0;
         }
         public void Deposit(double amount)
         {
--- UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Program.cs
+++ UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -7,32 +7,13 @@ namespace UpcastingDowncasting
     {
         static void Main(string[] args)
         {
-            //upcasting
-            Account account = new Account(1001, "Alex", 0.0);
-            BusinessAccount businessAccount = new BusinessAccount(1001, "maria", 50.0, 500);
-           
-            Account account1 = businessAccount;
-            Account account2 = new BusinessAccount(1003, "Janice", 0.0, 200.0);
-            Account account3 = new SavingsAccount(1004, "Megan", 0.0, 0.0);
-            //---------------------------------------//
+            Account account1 = new Account(1001, "Alex", 500.0);
+            Account account2 = new SavingsAccount(1002, "Maria", 500.0, 0.01);
 
-            //Downcasting
-            BusinessAccount account4 = (BusinessAccount)account2;
-            account4.Loan(100.0);
-            //error  BusinessAccount account5 = (BusinessAccount)account3; --> cannot convert from savings account to business account
-            if (account3 is BusinessAccount)
-            {
-                BusinessAccount account5 = (BusinessAccount)account3;
-                account5.Loan(200.0);
-                Console.WriteLine("Loan!");
-            }
-            if (account3 is SavingsAccount)
-            {
-                SavingsAccount account5 = (SavingsAccount)account3;
-                account5.UpdateBalance(200.0);
-                Console.WriteLine("Update!");
-            }
-
+            account1.Withdraw(10);
+            account2.Withdraw(10);
+            Console.WriteLine(account1.Balance);
+            Console.WriteLine(account2.Balance);
         }
     }
 }
